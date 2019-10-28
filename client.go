fec5c63ecc3d512fe66f3c1d22a33efb4c97e4b8
emahiro <hiromichi.a.ema@dena.com>
2019-10-28 16:34:53 +0900

    add clone header and bench sample method

--- client.go
+++ client.go
@@ -35,7 +35,42 @@ func (t *transport) RoundTrip(ireq *http.Request) (*http.Response, error) {
 	if req.Header == nil {
 		req.Header = make(http.Header)
 	}
+	req.Header = cloneHeader(req.Header)
 	req.Header.Set("Authorization", "Bearer "+token)
 
 	return t.base.RoundTrip(req)
+}
+
+func cloneHeader(h http.Header) http.Header {
+	nv := 0
+	for _, v := range h {
+		nv += len(v)
+	}
+
+	sv := make([]string, nv) // shared backing array for headers' values
+	h2 := make(http.Header, len(h))
+	for k, v := range h {
+		n := copy(sv, v)
+		h2[k] = sv[:n:n]
+		sv = sv[:n]
+	}
+	return h2
+}
+
+func cloneHeaderV1(h http.Header) http.Header {
+	h2 := make(http.Header, len(h))
+	for k, v := range h {
+		v2 := make([]string, len(v))
+		copy(v2, v)
+		h2[k] = v2
+	}
+	return h2
+}
+
+func cloneHeaderV2(h http.Header) http.Header {
+	h2 := make(http.Header, len(h))
+	for k, v := range h {
+		h2[k] = v
+	}
+	return h2
 }
