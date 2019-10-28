dd4ba935072218de4b110ece5ef0e78a24c4d6ec
emahiro <hiromichi.a.ema@dena.com>
2019-10-29 01:39:13 +0900

    modify TestRoundTrip
    create test server and request check

--- client_test.go
+++ client_test.go
@@ -2,17 +2,44 @@ package aehcl
 
 import (
 	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 func TestRoundTrip(t *testing.T) {
-	client := &http.Client{
-		Transport: Transport(http.DefaultTransport),
+	tests := []struct {
+		name    string
+		arg     http.RoundTripper
+		handler http.Handler
+	}{
+		{
+			name: "success to get authorization header",
+			arg:  Transport(http.DefaultTransport),
+			handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if h := r.Header.Get("Authorization"); h == "" {
+					t.Fatalf("Authroization Header is required")
+				}
+			}),
+		},
+		{
+			name: "faield to get authorization header",
+			arg:  &http.Transport{},
+			handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if h := r.Header.Get("Authorization"); h != "" {
+					t.Fatalf("Authroization Header is exist. header: %v", h)
+				}
+			}),
+		},
 	}
-	req, _ := http.NewRequest("GET", "/", nil)
-	client.Do(req)
 
-	if h := req.Header.Get("Authorization"); h != "" {
-		t.Fatal("Authorization shoud be concealed.")
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			client := &http.Client{
+				Transport: tt.arg,
+			}
+			server := httptest.NewServer(tt.handler)
+			client.Get(server.URL)
+		})
 	}
 }
